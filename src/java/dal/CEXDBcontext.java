80188326a5927d43d06d09dcf390fa75d6288399
dattt <88730526+abcdefghiklmnopq@users.noreply.github.com>
2022-03-01 22:51:03 +0700

    Create CEXDBcontext.java

---
+++ src/java/dal/CEXDBcontext.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+/**
+ *
+ * @author thand
+ */
+public class CEXDBcontext extends DBContext{
+    
+}
